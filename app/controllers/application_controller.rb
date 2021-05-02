880e237b1235a34ff183b379e40f8331b5fe0177
rata854 <nitiham2006@gmail.com>
2021-05-02 08:29:28 +0000

    [Add] devise_userを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # 始めにconfigure_permitted_parametersメソッドを起動。
+  # if: :devise_controller? → deviceにまつわる画面で起動。
+  
+  protected  
+  # protectedは1対Nの場合に使う？
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # email,passwordの他にnameデータの操作を追加している。
+    # （sign_up）の際に、ユーザ名（name）のデータ操作が許可。
+  end
+  
+  
+  
 end
