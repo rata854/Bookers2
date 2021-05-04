08c675f669e3908b099fc138377a7010c9e1c012
rata854 <nitiham2006@gmail.com>
2021-05-04 08:25:22 +0000

    [Add]booksコントローラ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+
   end
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @users = User.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice:"You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+     params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
+  
 end
