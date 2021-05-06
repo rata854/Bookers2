36b6c5bd76702606e0597a5675c152deb5cb2164
rata854 <nitiham2006@gmail.com>
2021-05-06 00:00:36 +0000

    [Add]user info作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @users = User.all
-
+    @user = current_user.id
+    @user = current_user.name
+    @user_introduction = current_user.introduction
   end
-
+  
+  def new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @users = User.all
+    if @user.save
+    redirect_to book_path(@user.id), notice:"successfully saved"
+    else
+      render :index
+    end
+  end
+  
   def show
     @user = User.find(params[:id])
-    @users = User.all
   end
-
+  
   def edit
     @user = User.find(params[:id])
   end
@@ -22,7 +38,13 @@ class UsersController < ApplicationController
     end
   end
   
-  private
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to books_path
+  end
+  
+  
   
   def user_params
      params.require(:user).permit(:name, :introduction, :profile_image_id)
